8a453225cd9dea82a8485a000405dccfa45d9bf2
Jeet Shah <62886607+jeet-shah@users.noreply.github.com>
2020-09-14 18:18:12 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+import WelcomeScreen from './screens/WelcomeScreen'
+
+export default function App() {
+  return (
+    <View>
+      <WelcomeScreen/>
+    </View>
+  );
+}
